b16f3a341f8e531801ad47055760cd24f14ed5a5
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-03-16 07:31:17 -0400

    Moving code over to laptop basically

--- src/data_manager/plotter.py
+++ src/data_manager/plotter.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+from numpy.linalg import norm
 from data_manager.data_grabber import split_data, load_data
 
 def plot(data):
-    [plt.plot(i) for i in data.tolist()]
+    [plt.plot(i/norm(i)) for i in data]
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from data_manager.data_grabber import split_data, load_data
 
 def main():
     training, testing = split_data(load_data('data_location.json'))
-    plot(training[0].EEG_data)
+    print(training[3].is_ADHD)
+    plot(training[3].EEG_data)
 
 if __name__ == '__main__':
     main()
