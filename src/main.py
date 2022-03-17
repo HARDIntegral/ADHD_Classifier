26020e8e3fd66cffbc7eb1caa4658a0815928679
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-03-17 02:29:30 -0400

    Found the frontal lobe channels

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from scipy.io import loadmat
+from random import choice
 from data_manager.plotter import plot
 from data_manager.data_grabber import split_data, bucket_data
 
 def main():
-    training, testing = split_data(bucket_data('data_location.json'))
-    print(training[2].is_ADHD)
-    plot(training[2].EEG_data)
+    patient, testing = split_data(bucket_data('data_location.json'))
+
+    patient = choice(patient)
+    print(patient.is_ADHD)
+    plot(patient.EEG_data[0:4] + patient.EEG_data[10:12] + patient.EEG_data[16:17])
 
 if __name__ == '__main__':
     main()
