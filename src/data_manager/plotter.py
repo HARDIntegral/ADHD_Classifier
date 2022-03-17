069ecc3e4fa3ce10b064248fa2f297d19e8d1e03
hard_integral <mohammed.a.tazwar04@gmail.com>
2022-03-17 10:29:53 -0400

    added plot niceness

--- src/data_manager/plotter.py
+++ src/data_manager/plotter.py
@@ -3,5 +3,6 @@ from numpy.linalg import norm
 from data_manager.data_grabber import split_data, bucket_data
 
 def plot(data):
-    [plt.plot(i/norm(i)) for i in data]
+    [plt.plot(i, label=f"Plot: {label}") for label,i in enumerate(data)]
+    plt.legend(['Fp1', 'Fp2', 'F3', 'F4', 'F7', 'F8', 'Fz'], loc=4)
     plt.show()
