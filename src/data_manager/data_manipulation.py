c891ac9a3425ae1fb2c6105c06e8ddf3c3edcfab
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-03-21 20:51:45 -0400

    Bug Fix: I stumbled across a major bug that incorrectly filtered the data

--- src/data_manager/data_manipulation.py
+++ src/data_manager/data_manipulation.py
@@ -6,10 +6,19 @@ def moving_average(data, avg_period):
 
 # Returns only the channels that read from the frontal lobe
 def restrict_frontal(data):
-    return data[0:4]+data[10:12]+data[16:17]
+    x = data[0:4].tolist()
+    y = data[10:12].tolist()
+    z = data[16:17].tolist()
+    return x+y+z
 
 def normalize(data):
     return data/np.linalg.norm(data)
+
+def labels(elements):
+    return [ 1 if i.is_ADHD else 0 for i in elements]
+
+def data_avg(data):
+    return sum(data)/len(data)
 
 #####################################################################
 # These are ideas as to what I can do to feed data into the models
@@ -18,10 +27,11 @@ def normalize(data):
 # Return the average slopes of the channels over time
 def avg_slope(elements, restrict=False, norm=False):
     slopes = [] 
-    if restrict:
-        elements = restrict_frontal(elements)
     for element in elements:
-        slopes += [ (i[-1] - i[0])/len(i) for i in (normalize(element.EEG_data) if norm else element.EEG_data) ]
+        slopes.insert(len(slopes), [                                                                                            \
+            (restrict_frontal(i)[-1] - restrict_frontal(i)[0])/len(restrict_frontal(i)) if restrict else (i[-1]-i[0])/len(i)    \
+            for i in (normalize(element.EEG_data) if norm else element.EEG_data)                                                \
+        ])
     return slopes
 
 # Second Idea:
@@ -29,10 +39,11 @@ def avg_slope(elements, restrict=False, norm=False):
 #   if that makes sense
 def avg_value(elements, restrict=False, norm=False):
     values = []
-    if restrict:
-        elements = restrict_frontal(elements)
     for element in elements:
-        values += [ sum(i)/len(i) for i in (normalize(element.EEG_data) if norm else element.EEG_data) ]
+        values.insert(len(values), [                                                                                            \
+            sum(restrict_frontal(i))/len(restrict_frontal(i)) if restrict else sum(i)/len(i)                                    \
+            for i in (normalize(element.EEG_data) if norm else element.EEG_data)                                                \
+        ])
     return values
 
 # Third Idea:
@@ -43,21 +54,22 @@ def second_gradient(list):
 
 def extremes(elements, sensitivity, restrict=False, norm=False):
     extremes = []
-    if restrict:
-        elements = restrict_frontal(elements)
-
     for element in elements:
         curve_extremes = []
-        upper_max = max([ max(second_gradient(i)) for i in (normalize(element.EEG_data) if norm else element.EEG_data) ])
-        lower_min = min([ min(second_gradient(i)) for i in (normalize(element.EEG_data) if norm else element.EEG_data) ])
+        upper_max = max([                                                                                                       \
+            max(second_gradient(restrict_frontal(i))) if restrict else max(second_gradient(i))                                  \
+            for i in (normalize(element.EEG_data) if norm else element.EEG_data)                                                \
+        ])
+        lower_min = min([                                                                                                       \
+            min(second_gradient(restrict_frontal(i))) if restrict else min(second_gradient(i))                                  \
+            for i in (normalize(element.EEG_data) if norm else element.EEG_data)                                                \
+        ])
         extr_sensitivity = (upper_max + lower_min)/(2*sensitivity)
-
         for i in (normalize(element.EEG_data) if norm else element.EEG_data):
             num_extremes = 0
-            for j in second_gradient(i):
+            for j in (second_gradient(restrict_frontal(i) if restrict else i)):
                 if abs(j)<extr_sensitivity:
                     num_extremes +=1
             curve_extremes.append(num_extremes)
-        extremes += curve_extremes
-
+        extremes.insert(len(extremes), (curve_extremes))
     return extremes
--- src/main.py
+++ src/main.py
@@ -1,29 +1,25 @@
 from scipy.io import loadmat
 from random import choice
-from data_manager.plotter import data_plotter_2d, data_plotter_3d, eeg_plot
+from data_manager.plotter import data_plotter_1d, data_plotter_2d, data_plotter_3d, eeg_plot
 from data_manager.data_grabber import split_data, bucket_data
 from data_manager.data_manipulation import avg_slope, avg_value, extremes, restrict_frontal
 
 def main():
     adhd, ctrl, testing = split_data(bucket_data('data_location.json'))
+
+    data_plotter_2d(avg_value(adhd, restrict=False, norm=True), avg_slope(adhd, restrict=False, norm=True), avg_value(ctrl, restrict=False, norm=True), avg_slope(ctrl, restrict=False, norm=True), labels = ['AVG Value', 'Extremes'], legend = ['ADHD', 'Control'])
     '''
-    #data_plotter(avg_slope(adhd, restrict=True, norm=True), avg_value(adhd, restrict=True, norm=True), avg_slope(ctrl, restrict=True, norm=True), avg_value(ctrl, restrict=True, norm=True))
-    #data_plotter(avg_slope(adhd, restrict=True, norm=True), extremes(adhd, 100, restrict=True, norm=True), avg_slope(ctrl, restrict=True, norm=True), extremes(ctrl, 100, restrict=True, norm=True))
-    
-    # this combination shows the best seperation
-    data_plotter_2d(avg_value(adhd, restrict=True, norm=True), extremes(adhd, 100, restrict=True, norm=True), avg_value(ctrl, restrict=True, norm=True), extremes(ctrl, 100, restrict=True, norm=True), labels = ['AVG Value', 'Extremes'], Legends = ['ADHD', 'Control'])
-    ''
     data_plotter_3d(    \
         avg_value(adhd, restrict=True, norm=True), avg_slope(adhd, restrict=True, norm=True), extremes(adhd, 100, restrict=True, norm=True),    \
         avg_value(ctrl, restrict=True, norm=True), avg_slope(ctrl, restrict=True, norm=True), extremes(ctrl, 100, restrict=True, norm=True),    \
-        labels = ['AVG Value', 'AVG Slope', 'Extremes'], Legends = ['ADHD', 'Control']  \
+        labels = ['AVG Value', 'AVG Slope', 'Extremes'], legend = ['ADHD', 'Control']  \
     )
-    ''
-    '''
+
     patient = choice(testing)
     print(patient.is_ADHD)
     eeg_plot(restrict_frontal(patient.EEG_data))
-    
+    '''
+    #data_plotter_1d(avg_value(adhd, restrict=True, norm=True)+avg_value(ctrl, restrict=True, norm=True), adhd+ctrl, label = 'AVG Value')
 
 if __name__ == '__main__':
     main()
