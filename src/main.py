3280ec8afc196d2ffaa6dcb639eb2107662ec5d3
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-03-21 23:27:28 -0400

    sad

--- src/main.py
+++ src/main.py
@@ -6,10 +6,12 @@ from data_manager.data_manipulation import RestrictType, avg_slope, avg_value, e
 
 def main():
     adhd, ctrl, testing = split_data(bucket_data('data_location.json'))
-    data_plotter_2d(
-        avg_value(adhd, restrict=RestrictType.IVRS, norm=True), extremes(adhd, 100, restrict=RestrictType.IVRS, norm=True), 
-        avg_value(ctrl, restrict=RestrictType.NORM, norm=True), extremes(ctrl, 100, restrict=RestrictType.NORM, norm=True), 
-        labels = ['AVG Value', 'Extremes'], legend = ['ADHD', 'Control']
+    
+    # Mess with this more, cannot find a good sperating pattern yet
+    data_plotter_3d(
+        avg_slope(adhd, restrict=RestrictType.IVRS, norm=False), avg_value(adhd, restrict=RestrictType.NONE, norm=False), extremes(adhd, 250, restrict=RestrictType.IVRS, norm=False),
+        avg_slope(ctrl, restrict=RestrictType.IVRS, norm=False), avg_value(ctrl, restrict=RestrictType.NONE, norm=False), extremes(ctrl, 250, restrict=RestrictType.IVRS, norm=False),
+        labels = ['AVG Slope', 'AVG Value', 'Extremes'], legend = ['ADHD', 'Control']
     )
     
 if __name__ == '__main__':
