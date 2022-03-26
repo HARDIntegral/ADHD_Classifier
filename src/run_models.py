c0f7370acfb9d39841775ebde2cec6480e992f86
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-03-26 11:17:29 -0400

    Well now I'm done

--- src/run_models.py
+++ src/run_models.py
@@ -4,18 +4,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
 def run_svm(data, labels, tests, test_labels):
-    clf = NuSVC(gamma="auto").fit(data, labels)
-    y_pred = clf.predict(tests)
-
-    print("\nSVM with RBF kernel\n")
-    print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
+    print('Support Vector Machines:')
+    for k in ['rbf', 'poly', 'sigmoid']:
+        y_pred = NuSVC(gamma='scale', kernel=k).fit(data, labels).predict(tests)
+        print(f'\nSVM with {k.upper()} kernel\n')
+        print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
 
 def run_knn(data, labels, tests, test_labels):
-    clf = KNeighborsClassifier(n_neighbors=3).fit(data, labels)
-    y_pred = clf.predict(tests)
-    
-    print("\nk-Nearest Neighbors\n")
-    print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
+    print('Nearest Neighbors:')
+    for k in range(3,8):
+        clf = KNeighborsClassifier(n_neighbors=k).fit(data, labels)
+        y_pred = clf.predict(tests)
+        print(f'\nk-Nearest Neighbors with {k} Voters\n')
+        print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
 
 def run_logreg(data, labels, tests, test_labels):
     clf = LogisticRegression(random_state=0).fit(data, labels)
