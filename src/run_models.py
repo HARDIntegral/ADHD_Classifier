ea19b6a186491207f276ab7ff827b8f8626de04a
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-03-26 00:21:35 -0400

    I guess I'm done now

--- src/run_models.py
+++ src/run_models.py
@@ -1,14 +1,25 @@
-from sklearn import svm
+from sklearn.svm import NuSVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
 def run_svm(data, labels, tests, test_labels):
-    clf = svm.NuSVC(gamma="auto")
-    clf.fit(data, labels)
+    clf = NuSVC(gamma="auto").fit(data, labels)
     y_pred = clf.predict(tests)
-    print(f'Accuracy is:{sum(test_labels==y_pred)/test_labels.shape[0]}')
+
+    print("\nSVM with RBF kernel\n")
+    print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
 
 def run_knn(data, labels, tests, test_labels):
-    clf = KNeighborsClassifier(n_neighbors=3)
-    clf.fit(data, labels)
+    clf = KNeighborsClassifier(n_neighbors=3).fit(data, labels)
     y_pred = clf.predict(tests)
-    print(f'Accuracy is:{sum(test_labels==y_pred)/test_labels.shape[0]}')
+    
+    print("\nk-Nearest Neighbors\n")
+    print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
+
+def run_logreg(data, labels, tests, test_labels):
+    clf = LogisticRegression(random_state=0).fit(data, labels)
+    y_pred = clf.predict(tests)
+
+    print("\nLogistic Regression\n")
+    print(classification_report(test_labels, y_pred, target_names=['Control', 'ADHD']))
