9f642de97bb322bad968a476d5a11d0ca5319a51
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2022-04-03 12:20:17 -0400

    Removed more dead code

--- src/data_manager/data_manipulation.py
+++ src/data_manager/data_manipulation.py
@@ -4,7 +4,6 @@ import numpy as np
 class RestrictType(Enum):
     NONE = 0
     NORM = 1
-    IVRS = 2
 
 #################################################################################################################################
 # These are helper functions
@@ -19,19 +18,9 @@ def restrict_frontal(data, restrict=RestrictType.NORM):
         x = data[0:4].tolist()
         y = data[10:12].tolist()
         z = data[16:17].tolist()
-    elif restrict == RestrictType.IVRS:
-        x = data[4:10].tolist()
-        y = data[12:16].tolist()
-        z = data[17:-1].tolist()
     else:
         return data
     return x+y+z
-
-def normalize(data):
-    return data/np.linalg.norm(data)
-
-def labels(elements):
-    return [ 1 if i.is_ADHD else 0 for i in elements]
 
 def data_avg(data):
     return sum(data)/len(data)
@@ -41,23 +30,23 @@ def data_avg(data):
 
 # First Idea:
 # Return the average slopes of the channels over time
-def avg_slope(elements, restrict=RestrictType.NONE, norm=False):
+def avg_slope(elements, restrict=RestrictType.NONE):
     slopes = [] 
     for element in elements:
         slopes.append([                                                                                                         \
             round((restrict_frontal(i, restrict)[-1] - restrict_frontal(i, restrict)[0])/len(restrict_frontal(i, restrict)), 2) \
-            for i in (normalize(element.EEG_data) if norm else element.EEG_data)                                                \
+            for i in element.EEG_data                                                                                           \
         ])
     return slopes
 
 # Second Idea:
 # Return the average value of the values of a channel over time
 #   if that makes sense
-def avg_value(elements, restrict=RestrictType.NONE, norm=False):
+def avg_value(elements, restrict=RestrictType.NONE):
     values = []
     for element in elements:
         values.append([                                                                                                         \
             round(sum(restrict_frontal(i, restrict))/len(restrict_frontal(i, restrict)), 2)                                     \
-            for i in (normalize(element.EEG_data) if norm else element.EEG_data)                                                \
+            for i in element.EEG_data                                                                                           \
         ])
     return values
--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
-import numpy as np
-from scipy.io import loadmat
-from data_manager.plotter import data_plotter_1d, data_plotter_2d, data_plotter_3d, eeg_plot
 from data_manager.data_grabber import split_data, bucket_data
-from data_manager.data_manipulation import RestrictType, labels, data_avg, avg_slope, avg_value, extremes, restrict_frontal
+from data_manager.data_manipulation import RestrictType, data_avg, avg_slope, avg_value
 
 def main():
     # load the data
