7cda99d88166ed280abeaaf445428fb953859025
integral <mohammed.a.tazwar04@gmail.com>
2022-04-14 20:36:09 -0400

    Finished writing the code, need to run tests but pc keeps crashing so I gotta do them on my laptop

--- run_test.sh
+++ run_test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-for (( i=0; i <25; i++ ))
+for (( i=1; i < 26; i++ ))
 do
     echo "RBF KERNEL MODEL ${i}"
-    python src/main.py 1
-done
-
-for (( i=0; i <25; i++ ))
-do
-    echo "CUSTOM KERNEL MODEL ${i}"
-    python src/main.py 0
+    python src/main.py $1
 done
