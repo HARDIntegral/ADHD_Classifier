3d1dd743bd082ba648b4dbf9f75c1d3b06314f98
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-04-16 02:44:35 -0400

    Made Test Executable From Anywhere

--- Tools/Test.sh
+++ Tools/Test.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-for (( i=1; i < 251; i++ ))
+root=$(dirname $0)
+main="${root}/../src/main.py"
+
+for (( i=1; i < 51; i++ ))
 do
     echo "TRIAL # ${i}"
-    python3 ../src/main.py
+    python3 "$main"
 done
