2133eba90ec4b67aaf8c22d7e0e3b1fa1036175e
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-04-25 17:24:03 -0400

    Refactored Main

--- Source/main.py
+++ Source/main.py
@@ -5,22 +5,35 @@ from data_manager.data_manipulation import avg_slope, avg_value
 from SVM.svm import SVM
 
 def main():
+    
     # load the data
-    adhd, ctrl = bucket_data('Data/Locations.json')
+    
+    adhd , control = bucket_data('Data/Locations.json')
 
     # assign features to the data
-    for e, d in zip(adhd, list(zip(avg_slope(adhd), avg_value(adhd)))):
-        e.add_features(d[0] + d[1])
-    for e, d in zip(ctrl, list(zip(avg_slope(ctrl), avg_value(ctrl)))):
-        e.add_features(d[0] + d[1])
-
+    
+    assignFeatures(adhd)
+    assignFeatures(control)
+    
     # split data into training and testing sets
-    training, testing = split_data(adhd, ctrl)
-    model = SVM(training, testing)
-    model.fit(1, 1)
+    
+    training , testing = split_data(adhd,control)
+    
+    model = SVM(training,testing)
+    model.fit(1,1)
     model.test()
-    model.fit(0, 1)
+    model.fit(0,1)
     model.test()
+
+
+def assignFeatures(dataset):
+    for element , features in zip(dataset,packFeatures(dataset)):
+        element.add_features(features[0] + features[1])
+        
+
+def packFeatures(dataset):
+    return list(zip(avg_slope(dataset),avg_value(dataset)))
+
 
 if __name__ == '__main__':
     main()
