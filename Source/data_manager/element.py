33e0c1d3006385cf576fbfe7115046cd2f61df0d
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-04-25 18:21:58 -0400

    Ported Python Refactorings

--- Source/data_manager/element.py
+++ Source/data_manager/element.py
@@ -1,8 +1,23 @@
-# A wrapper for a element in either the training or testing set
+
 class Element():
+    '''
+    A wrapper for an element of the training / testing set
+    
+    Attributes
+    ----------
+    is_ADHD : bool
+        Whether of not this element represents a child with ADHD or not
+    EEG_data : numpy array
+        The EEG recording in the format of a numpy array
+    
+    '''
+
+
     def __init__(self, is_ADHD, EEG_data):
-        self.is_ADHD = is_ADHD      # whether or not this element represents a child with ADHD or not
-        self.EEG_data = EEG_data    # the EEG recording in the format of a numpy array 
+        self.is_ADHD = is_ADHD
+        self.EEG_data = EEG_data
+
 
     def add_features(self, features):
-        self.features = features    # once their features are extracted, the features are assigned to themselves
+        '''once their features are extracted, the features are assigned to themselves'''
+        self.features = features
